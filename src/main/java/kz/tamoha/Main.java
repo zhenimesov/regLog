6799042179243f420d22befdcbe160e9110e1282
tamerlan.zhenimesov <zhenimesov121104@gmail.com>
2024-04-29 18:50:35 +0500

    firstcommit

--- src/main/java/kz/tamoha/Main.java
+++ src/main/java/kz/tamoha/Main.java
@@ -1,21 +1,10 @@
 package kz.tamoha;
-
-import com.google.gson.Gson;
 import kz.tamoha.auth.AuthOrReg;
-import kz.tamoha.basic.model.DataBaseModel;
-import kz.tamoha.basic.model.User;
-import kz.tamoha.database.DataBaseGson;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
     AuthOrReg authOrReg = new AuthOrReg();
     authOrReg.getAuth();
 
-
-
-        System.out.println();
     }
 }
